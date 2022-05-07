1d6e499708a1b4c7e054ea6237a3cf930f396a57
Pawel <pkownacki90@gmail.com>
2022-05-07 16:49:06 +0200

    Gotowe zad1 lista3

--- lista3/zad1.cpp
+++ lista3/zad1.cpp
@@ -101,15 +101,24 @@ class StudentEdukacja: public Student
     friend class studentSort;
 private:
     int numer_indeksu;
+    void setNrIndeksu(int nr_indeksu) {
+        this->numer_indeksu = nr_indeksu;
+    }
+    int getNrIndeksu() {
+        return numer_indeksu;
+    }
 public:
-   // void podaj_numer_indeksu() {
-   // };
-    StudentEdukacja(string imie, string nazwisko, int wiek, string grupa, int numer_indesku){
+   int podaj_numer_indeksu() {
+        int nr_indeksu;
+        cout << "Podaj nr indeksu: ";
+        cin >> nr_indeksu;
+        return nr_indeksu;
+    };
+    StudentEdukacja(string imie, string nazwisko, int wiek, string grupa){
         setImie(imie);
         setNazwisko(nazwisko);
         setWiek(wiek);
         setGrupa(grupa);
-        this->numer_indeksu = numer_indeksu;
     };
     StudentEdukacja(){};
     
@@ -118,22 +127,36 @@ class studentSort {
     private:
         StudentEdukacja students[N];
         void sortArray(){
-
+            StudentEdukacja buf;
+            int k;
+            for(k=0;k<N-1;k++)
+                for(int j=k+1;j<N;j++)
+                    if(students[k].getNrIndeksu()>students[j].numer_indeksu){
+                        buf=students[k];
+                        students[k]=students[j];
+                        students[j]=buf;
+            }
         };
     public:
         void fillArray(){
             srand(time(NULL));
+            int nr_indeksu;
             for (int i=0; i<N; i++) { 
-                students[i] = StudentEdukacja("Jan", "Kowalski", rand()%99, "Programowanie", rand());
+                int ranWiek = rand()% 30 + 25;
+                students[i] = StudentEdukacja("Jan", "Kowalski", ranWiek, "Programowanie");
+                cout << "Podaj nr indeksu: ";
+                cin >> nr_indeksu;
+                students[i].setNrIndeksu(nr_indeksu);
             }
         };
         void showArray() {
+            sortArray();
             for (int i=0;i<N;i++) { 
                 cout << students[i].getImie() 
                 << " "  << students[i].getNazwisko() 
                 << " "  << students[i].getWiek() 
                 << " "  << students[i].getGrupa()
-                << " "  << students[i].numer_indeksu
+                << " "  << students[i].getNrIndeksu()
                 << endl;
             }
         }
